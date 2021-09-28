775c40f1cef6cc1aecbe7e540232ef3bb60e9a7d
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-09-28 12:54:36 -0700

    couple events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+
+// getters
+
+const header = document.querySelector('header');
+const navLinks = document.querySelectorAll('.nav-link');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// event listeners
+header.addEventListener('mouseenter', e => e.target.style.backgroundColor = 'yellow');
+header.addEventListener('mouseleave', e => e.target.style.backgroundColor = 'white');
+
+navLinks.forEach(link => link.addEventListener('mouseenter', e => e.target.style.textDecoration = 'underline'));
+navLinks.forEach(link => link.addEventListener('mouseleave', e => e.target.style.textDecoration = ''));
+
